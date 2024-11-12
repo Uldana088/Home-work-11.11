e140b9bdece02fa0b60bc864429bb0be3fd5170f
Abdiomar Uldana <Abdiumar@list.ru>
2024-11-12 14:55:35 +0500

    Home work 11.11

---
+++ 12.11.js
@@ -0,0 +1,48 @@
+// exercise1
+var a = Number(prompt("Бірінші қабырға санын енгізіңіз:"));
+var b = Number(prompt("Екінші қабырға санын енгізіңіз"));
+var c = Number(prompt("Үшінші қабырға санын енгізіңіз:"));
+
+if (a + b > c && a + c > b && b + c > a) {
+  alert("Бұл үшбұрышты жасай алады");
+} else {
+  alert("Бұл үшбұрышты жасай алмайды");
+}
+
+
+//exercise 2
+let question = prompt ("0 немесе одан жогары бутын сан енгізіңіз:");
+let result = 1;
+    switch (true) {
+        case ( question < 0):
+            alert("Факториалды тек 0 немесе одан жоғары сандар үшін есептей аламыз");
+            break;
+        case (question === 0):
+            alert("Санның факториалы: 1");
+            break;
+        default:
+            for (let i = 1; i <= question; i++) {
+                result *= i;
+            }
+            alert("Санның факториалы:result");
+            break;
+    }
+
+// exercise3
+let gradee = prompt("0-ден 100-ге дейін ұпай енгізіңіз:");
+
+if (gradee >= 0 && gradee <= 59) {
+  alert("Баға: F");
+}
+if (gradee >= 60 && gradee <= 69) {
+  alert("Баға: D");
+}
+if (gradee >= 70 && gradee <= 79) {
+  alert("Баға: C");
+}
+if (gradee >= 80 && gradee <= 89) {
+  alert("Баға: B");
+}
+if (gradee >= 90 && gradee <= 100) {
+  alert("Баға: A");
+}
